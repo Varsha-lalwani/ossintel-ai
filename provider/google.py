cf558a0e6bfeef21a8c361280e5a54d24e15faf6
Varsha-lalwani <varsha.lalwani@harness.io>
2024-05-30 08:40:00 +0530

    Fix prompt

--- provider/google.py
+++ provider/google.py
@@ -22,8 +22,8 @@ class VertexAI:
              top_p: float,
              top_k: int,
              ) :
-        instructions = "You are a package searching engine.\
-            You will list the packages based on the inputs.\
+        instructions = "You are a oss packages searching engine.\
+            You will list the packages based on the requirement.\
             Return the result as a JSON. \
             The response format should be as follows:\
             Don't return duplicates. \
@@ -40,14 +40,18 @@ class VertexAI:
             }\
             The response should be a JSON Object with a key \"packages\" and value as an array of JSON objects. \
             Each JSON object should have two keys \"packageName\" and \"repositoryUrl\". \
-            The packages array  should contain as many packages as available for the usecase but max 15 packages based with no duplicates. \
+            The packages list shouldn't have any duplicates. \
+            The packages array should contain as many packages as available for the usecase with a upper limit of 10 packages.\
             Ensure that both \"packageName\" and \"repositoryUrl\" do not include version information. \
+            Don't return more than 1 entry from a single person. \
             Don't return json in any other format."
         model = GenerativeModel(model_name,
                                  system_instruction=[instructions])
-        
+        newPrompt = add_ecosystem(prompt, ecosystem)
+        print(newPrompt)
+        print(prompt)
         response = model.generate_content(
-            "In" + ecosystem +prompt,
+            newPrompt,
             generation_config=GenerationConfig(temperature=temperature,
                                                 max_output_tokens=max_output_tokens,
                                                 top_p=top_p,
@@ -59,6 +63,16 @@ class VertexAI:
         str = str.replace("json", "")
         return json.loads(str)
 
-
+def add_ecosystem (prompt: str, ecosystem: str):
+    if (ecosystem == "NPM"):
+        return "For JS " + prompt
+    elif (ecosystem == "MAVEN"):
+        return "For Java " + prompt
+    elif (ecosystem == "PYPI"):
+        return "For Python " + prompt
+    elif (ecosystem == "GO"):
+        return "For Go " + prompt
+    else :
+        return "For " + ecosystem + " " + prompt
 
 
